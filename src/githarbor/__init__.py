e59818ac010448f521175d623d916f25a671193d
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-04-22 07:05:59 +0200

    Bump version 0.7.1 → 0.8.0

--- src/githarbor/__init__.py
+++ src/githarbor/__init__.py
@@ -22,7 +22,7 @@ from githarbor.exceptions import (
 )
 from githarbor.repositories import create_repository
 
-__version__ = "0.7.1"
+__version__ = "0.8.0"
 
 __all__ = [
     "AuthenticationError",
